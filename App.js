af2b751aee8e72260827b10fbbd4be36076dfd0e
fahad <90055525+fahad0samara@users.noreply.github.com>
2023-08-18 07:46:41 +0300

    frist commit and add smoe screen and the navigation

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
-export default function App() {
+
+export default function WelcomeScreen() {
+  const navigation = useNavigation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 
+      'black', justifyContent: 'center', alignItems: 'center'
+
+     }}>
+      <View style={{ flex: 1, justifyContent: 'space-around', alignItems: 'center', marginTop: 32 }}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 32, textAlign: 'center' }}>
+          Let's Get Started!
+        </Text>
+        <View style={{ flexDirection: 'row', justifyContent: 'center' }}>
+         
+        </View>
+        <View style={{ marginBottom: 32 }}>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUp')}
+            style={{
+              paddingVertical: 12,
+              backgroundColor: 'yellow',
+              marginHorizontal: 16,
+              borderRadius: 10,
+            }}
+          >
+            <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', color: 'gray' }}>
+              Sign Up
+            </Text>
+          </TouchableOpacity>
+          <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 16 }}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>Already have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text style={{ fontWeight: 'bold', color: 'yellow' }}> Log In</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
